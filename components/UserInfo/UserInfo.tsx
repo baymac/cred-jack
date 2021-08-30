393b8a6504e505cee36d80d65142052310cef522
Parichay Barpanda <parichay.barpanda@gmail.com>
2021-08-30 18:52:21 +0530

    Show user info

--- components/UserInfo/UserInfo.tsx
+++ components/UserInfo/UserInfo.tsx
@@ -1,8 +1,11 @@
 import cn from 'classnames';
+import useUser from '../../lib/useUser';
 import rootStyles from '../../styles/root.module.css';
 import styles from './userinfo.module.css';
 
 export default function UserInfo() {
+  const { user } = useUser();
+
   return (
     <section
       className={cn(rootStyles.section, styles.about__section)}
@@ -15,7 +18,28 @@ export default function UserInfo() {
           styles.about__container
         )}
       >
-        {JSON.stringify(1)}
+        <div className={styles.item_wrapper}>
+          <div className={styles.item}>
+            <p>
+              <b>Name:</b> {user.first_name} {user.last_name}
+            </p>
+          </div>
+          <div className={styles.item}>
+            <p>
+              <b>Phone:</b> {user.phone}
+            </p>
+          </div>
+          <div className={styles.item}>
+            <p>
+              <b>Email:</b> {user.email}
+            </p>
+          </div>
+          <div className={styles.item}>
+            <p>
+              <b>Solana Address:</b> {user.sol_addr}
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
